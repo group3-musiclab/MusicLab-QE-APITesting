f8151fe98a56173e78bedf7f2e44bc5c68494513
alandarma <alandarmasaputra667@gmail.com>
2023-03-30 08:12:29 +0700

    Review

---
+++ src/test/java/MusicLab/StepDefinitions/Review/PostCreateReviewinMentorWithInvalidStepdefs.java
@@ -0,0 +1,27 @@
+package MusicLab.StepDefinitions.Review;
+
+import MusicLab.API.Review.ReviewAPI;
+import MusicLab.utilities.Instrument.ReqBodyInstrument;
+import MusicLab.utilities.Review.ReqBodyReview;
+import io.cucumber.java.en.Given;
+import net.thucydides.core.annotations.Steps;
+
+import java.io.File;
+
+public class PostCreateReviewinMentorWithInvalidStepdefs {
+
+    @Steps
+    ReviewAPI reviewAPI ;
+
+    @Given("Post create Review with invalid JSON")
+    public void postCreateReviewWithInvalidJSON() {
+        File jsonReq = new File(ReqBodyReview.REQ_BODY + "PostCreateReviewinMentorWithInvalid.json");
+        ReviewAPI.setPostCreateReviewInMentorWithInvalid(jsonReq) ;
+    }
+
+    @Given("Post create Review with empty comment")
+    public void postCreateReviewWithEmptyComment() {
+        File jsonReq = new File(ReqBodyReview.REQ_BODY + "PostCreateReviewinMentorWithempty.json");
+        ReviewAPI.setPostCreateReviewInMentorWithInvalid(jsonReq) ;
+    }
+}
